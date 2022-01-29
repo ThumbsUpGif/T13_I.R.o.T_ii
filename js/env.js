e2d74bc6cfc1089d729d031432609dbedaafa70c
Magnus J <74900307+ThumbsUpGif@users.noreply.github.com>
2022-01-29 13:11:13 +0100

    commit- first commit, complete CSS but no JS

---
+++ js/env.js
@@ -0,0 +1,9 @@
+// her definer noen generisk variabler: den første er cdn (content distribution network)
+// her inne sanity lagre bilder vi lastet opp i sanity platform
+export const projectID = 'p5snqp28'
+export const cdnUrl = `https://cdn.sanity.io/images/${projectID}/production/`;
+
+
+// carlo sitt projectID: 'p5snqp28'
+
+// I.R.o.T. projectID: 'aav6p9fi'
